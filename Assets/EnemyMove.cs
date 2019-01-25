05c4beaf266939262a1778dd855fed90a2258d3c
Unknown <lewis2.bond@live.uwe.ac.uk>
2019-01-25 22:00:15 +0000

    Enemy spawning
    Added basic enemy spawning and started enemy movement

---
+++ Assets/EnemyMove.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour {
+
+
+    private GameObject enemy;
+    private int target_waypoint = 6;
+    private int current_waypoint = 0;
+    private GameObject[] waypoints;
+    float[] difference = new float[] { };
+
+    // Use this for initialization
+    void Start () {
+        if (waypoints == null)
+            waypoints = GameObject.FindGameObjectsWithTag("Waypoint");
+
+        enemy = this.GetComponent<GameObject>();
+
+        for(int i = 0; i < waypoints.Length; i++)
+        {
+            difference[i] = Mathf.Sqrt((enemy.transform.position.x * waypoints[i].transform.position.x) + (enemy.transform.position.y * waypoints[i].transform.position.y));
+        }
+
+        for (int i = 0; i < difference.Length; i++)
+        {
+            for (int j = 0; j < difference.Length; j++)
+            {
+                if(difference[i] < difference[j])
+                {
+                    current_waypoint = i;
+                }
+            }
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+	}
+}
---
+++ Assets/Scripts/SpawnEnemy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemy : MonoBehaviour {
+
+    private int enemy_count = 0;
+    private GameObject enemy;
+    private GameObject[] spawnPoints;
+    float timer = 0.0f;
+
+
+    void Awake()
+    {
+        enemy = GameObject.FindGameObjectWithTag("Enemy");
+        if(spawnPoints == null)
+            spawnPoints = GameObject.FindGameObjectsWithTag("SpawnPoint");
+    }
+
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        timer += Time.deltaTime;
+
+        if(timer > 1.0f)
+        {
+            timer = 0.0f;
+            Instantiate(enemy, spawnPoints[Random.RandomRange(0, spawnPoints.Length)].transform.position, Quaternion.identity);
+            enemy_count++;
+        }
+	}
+}
