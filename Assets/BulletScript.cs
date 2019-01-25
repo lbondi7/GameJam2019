a09c1cb7c8bc260c79cc1a52f04162350efd19bf
Tom Southern <tom2.southern@live.uwe.ac.uk>
2019-01-25 22:44:58 +0000

    turret fireing every three seconds
    Next need to do collison of the projectile

---
+++ Assets/BulletScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour {
+
+    public float bullet_x_velocity = 5f;
+    float bullet_y_velocity = 0f;
+
+    Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rb.velocity = new Vector2(bullet_x_velocity, bullet_y_velocity);
+        Destroy(gameObject, 3f);
+	}
+}
---
+++ Assets/TurretFire.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretFire : MonoBehaviour {
+
+    public GameObject bulletToRight, bulletToLeft;
+    Vector2 bulletPos;
+    float timer;
+    int seconds;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        timer += Time.deltaTime;
+        seconds = (int)timer;
+        Debug.Log(seconds);
+
+        if (seconds == 3/*SECONDS*/)
+        {
+            Fire();
+
+            Debug.Log("FIRE");
+            bulletPos = transform.position;
+            Instantiate(bulletToRight, bulletPos, Quaternion.identity);
+            timer = 0;
+        }
+
+	}
+
+    void Fire()
+    {
+
+    }
+}
