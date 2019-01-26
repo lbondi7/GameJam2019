26d25f36a67ff1a3d10e90a51a8bbf581819ea08
Unknown <jack2.lynham@live.uwe.ac.uk>
2019-01-26 19:24:34 +0000

    Lynnys Stuff

---
+++ Assets/Point_UI.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Point_UI : MonoBehaviour
+{
+
+    private int count;
+
+    public Text lovePoints;
+
+    private void Start()
+    {
+        count = 0;
+        SetCountText();
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+
+
+    void SetCountText()
+
+    {
+        lovePoints.text = "Count: " + count.ToString();
+    }
+}
+
+
