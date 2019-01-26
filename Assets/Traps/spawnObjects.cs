7d8ab93646d7c7a010baaa5d403ab3e4e5d7361f
Kingy <jack3.king@live.uwe.ac.uk>
2019-01-26 23:11:03 +0000

    UI Added

--- Assets/Traps/spawnObjects.cs
+++ Assets/Traps/spawnObjects.cs
@@ -9,19 +9,24 @@ public class spawnObjects : MonoBehaviour {
     public GameObject sticky_object;
     public GameObject gaurddog_object;
     public Camera camera;
+    public GameObject player;
 
-    public GameObject[] trap_array;
+
 
     // Use this for initialization
-    public enum TrapState
+    public enum ClickState
     {
         NONE = -1,
         SPIKES = 0,
         STICKY = 1,
-        GAURDDOG = 2
+        GAURDDOG = 2,
+        BAT = 3,
+        SPATULA = 4,
+        GUITAR = 5,
+        HAMMER = 6
     }
 
-    public TrapState currently_selected_trap = TrapState.NONE;
+    public ClickState currently_selected_trap = ClickState.NONE;
 
 	void Start ()
     {
@@ -35,39 +40,57 @@ public class spawnObjects : MonoBehaviour {
 
         transform.position = Camera.main.ScreenToViewportPoint(Input.mousePosition);
 
-        if (Input.GetMouseButtonDown(0) && currently_selected_trap == TrapState.SPIKES)
+        if (Input.GetMouseButtonDown(0) && currently_selected_trap == ClickState.SPIKES)
         {
-            GameObject newObj = (GameObject)Instantiate(spike_object, camera.ScreenToWorldPoint(Input.mousePosition), Quaternion.identity) as GameObject;
-            newObj.transform.position = new Vector3(camera.ScreenToWorldPoint(Input.mousePosition).x, camera.ScreenToWorldPoint(Input.mousePosition).y, 0);
+            //GameObject newObj = (GameObject)Instantiate(spike_object, camera.ScreenToWorldPoint(Input.mousePosition), Quaternion.identity) as GameObject;
+            //newObj.transform.position = new Vector3(camera.ScreenToWorldPoint(Input.mousePosition).x, camera.ScreenToWorldPoint(Input.mousePosition).y, 0);
         }
 
-        if (Input.GetMouseButtonDown(0) && currently_selected_trap == TrapState.STICKY)
+        if (Input.GetMouseButtonDown(0) && currently_selected_trap == ClickState.STICKY)
         {
-            GameObject newObj = (GameObject)Instantiate(sticky_object, camera.ScreenToWorldPoint(Input.mousePosition), Quaternion.identity) as GameObject;
-            newObj.transform.position = new Vector3(camera.ScreenToWorldPoint(Input.mousePosition).x, camera.ScreenToWorldPoint(Input.mousePosition).y, 0);
+            //GameObject newObj = (GameObject)Instantiate(sticky_object, camera.ScreenToWorldPoint(Input.mousePosition), Quaternion.identity) as GameObject;
+            //newObj.transform.position = new Vector3(camera.ScreenToWorldPoint(Input.mousePosition).x, camera.ScreenToWorldPoint(Input.mousePosition).y, 0);
         }
 
-        if (Input.GetMouseButtonDown(0) && currently_selected_trap == TrapState.GAURDDOG)
+        if (Input.GetMouseButtonDown(0) && currently_selected_trap == ClickState.GAURDDOG)
         {
-            GameObject newObj = (GameObject)Instantiate(gaurddog_object, camera.ScreenToWorldPoint(Input.mousePosition), Quaternion.identity) as GameObject;
-            newObj.transform.position = new Vector3(camera.ScreenToWorldPoint(Input.mousePosition).x, camera.ScreenToWorldPoint(Input.mousePosition).y, 0);
+            //GameObject newObj = (GameObject)Instantiate(gaurddog_object, camera.ScreenToWorldPoint(Input.mousePosition), Quaternion.identity) as GameObject;
+            //newObj.transform.position = new Vector3(camera.ScreenToWorldPoint(Input.mousePosition).x, camera.ScreenToWorldPoint(Input.mousePosition).y, 0);
         }
     }
 
     public void setSpikeActive()
     {
-        currently_selected_trap = TrapState.SPIKES;
+        currently_selected_trap = ClickState.SPIKES;
     }
 
     public void setStickyActive()
     {
-        currently_selected_trap = TrapState.STICKY;
+        currently_selected_trap = ClickState.STICKY;
     }
 
     public void setGaurdDogActive()
     {
-        currently_selected_trap = TrapState.GAURDDOG;
+        currently_selected_trap = ClickState.GAURDDOG;
     }
 
+    public void setHammerActive()
+    {
+        currently_selected_trap = ClickState.HAMMER;
+    }
 
+    public void setSpatulaActive()
+    {
+        currently_selected_trap = ClickState.SPATULA;
+    }
+
+    public void setGuitarActive()
+    {
+        currently_selected_trap = ClickState.GUITAR;
+    }
+
+    public void setBatActive()
+    {
+        currently_selected_trap = ClickState.BAT;
+    }
 }
---
+++ Assets/UI/Moneytostring.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Moneytostring : MonoBehaviour {
+
+    public Text money_text_ui; // assign it from inspector
+    public string front_text;
+    public PlayerManager player_manager;
+
+    // Update is called once per frame
+    void Update()
+    {
+        string moneytostring = front_text + player_manager.money.ToString();
+        money_text_ui.text = moneytostring;
+    }
+}
