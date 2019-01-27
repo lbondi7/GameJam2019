fe74a0ec9e38d440da83a2f5bb5df18da0207564
Tom Southern <tom2.southern@live.uwe.ac.uk>
2019-01-27 15:59:06 +0000

    Enemy Porjectile collison IN

--- Assets/GetHitScript.cs
+++ Assets/GetHitScript.cs
@@ -4,46 +4,23 @@ using UnityEngine;
 
 public class GetHitScript : MonoBehaviour {
 
-    bool slowed = false;
-    public int slowed_timer;
-
-    float timer;
-    int seconds;
-
-
     // Use this for initialization
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (slowed)
-        {
-            timer -= Time.deltaTime;
-            seconds = (int)timer;
-
-            if (seconds == 0)
-            {
-                slowed = false;
-            }
-            else
-            {
-                transform.Rotate(Vector3.forward);
-            }
-        }
 
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (!slowed)
+        if (collision.gameObject.tag == "Projectile")
         {
-            timer = slowed_timer;
-            slowed = true;
+            Destroy(gameObject, 0f);
         }
-
     }
 }
