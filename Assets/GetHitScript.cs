6f7507d5ca1e97b0520c7e39bc4834c43a913ec5
Tom Southern <tom2.southern@live.uwe.ac.uk>
2019-01-27 02:37:00 +0000

    finished Nerf and Paint, further functionality gigging is required

--- Assets/GetHitScript.cs
+++ Assets/GetHitScript.cs
@@ -44,5 +44,6 @@ public class GetHitScript : MonoBehaviour {
             timer = slowed_timer;
             slowed = true;
         }
+
     }
 }
--- Assets/TurretFire.cs
+++ Assets/TurretFire.cs
@@ -12,7 +12,6 @@ public class TurretFire : MonoBehaviour {
     float timer;
     int seconds;
 
-
 	// Use this for initialization
 	void Start () {
 		
